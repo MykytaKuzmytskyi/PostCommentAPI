84af188e30d325322c365d6c22c6d261f686d841
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-20 21:05:32 +0300

    Created models & schemas for user app.

---
+++ src/user/models.py
@@ -0,0 +1,15 @@
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import Integer, Column, Boolean, Interval
+from sqlalchemy.orm import relationship
+
+from database.database import Base
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True, index=True)
+
+    auto_reply_enabled = Column(Boolean, default=False)  # Включить/выключить автоответы
+    auto_reply_delay = Column(Interval, nullable=True)
+
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
