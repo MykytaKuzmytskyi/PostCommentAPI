3d386eda60a49e84102f0008bd77255179a8edad
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-24 23:14:34 +0300

    Update test_post.

--- tests/test_post.py
+++ tests/test_post.py
@@ -6,13 +6,9 @@ post_fields = ["title", "content", "id", "created_at", "user_id"]
 @pytest.mark.asyncio
 class TestAsyncClient:
     async def test_user_me(self, auth_client):
-        # Запрос к /users/me
         response = await auth_client.get("/users/me")
-
-        # Проверка, что запрос выполнен успешно
         assert response.status_code == 200, "Failed to fetch user data"
 
-        # Проверка, что данные пользователя возвращаются
         user_data = response.json()
         assert "email" in user_data, "User data should contain email"
         assert (
@@ -22,27 +18,11 @@ class TestAsyncClient:
         response = await auth_client.get("/users")
         assert response.status_code == 200
 
-    async def test_create_post(self, auth_client):
-        data = {"title": "New Test Post", "content": "New Test content"}
-        response = await auth_client.post("/posts", json=data)
-        response_data = response.json()
-
-        assert response.status_code == 201
-        assert "id" in response_data, "ID not found in response"
-
-        for key in data:
-            assert key in response_data, f"Key '{key}' not found in response"
-
-        assert (
-            response_data["title"] == data["title"]
-        ), f"Expected title to be '{data['title']}', but got '{response_data['title']}'"
-        assert response_data["content"] == data["content"]
-
     async def test_get_posts(self, auth_client):
         response = await auth_client.get("posts")
         assert response.status_code == 200
         response_data = response.json()
-        assert len(response_data) == 1
+        assert len(response_data) == 2
         for key in response_data[0]:
             assert key in post_fields
 
@@ -76,3 +56,19 @@ class TestAsyncClient:
         response = await auth_client.get(f"/post/1")
         assert response.status_code == 404
         assert response.json()["detail"] == f"The post with id 1 does not exist"
+
+    async def test_create_post(self, auth_client):
+        data = {"title": "New Test Post", "content": "New Test content"}
+        response = await auth_client.post("/posts", json=data)
+        response_data = response.json()
+
+        assert response.status_code == 201
+        assert "id" in response_data, "ID not found in response"
+
+        for key in data:
+            assert key in response_data, f"Key '{key}' not found in response"
+
+        assert (
+            response_data["title"] == data["title"]
+        ), f"Expected title to be '{data['title']}', but got '{response_data['title']}'"
+        assert response_data["content"] == data["content"]
