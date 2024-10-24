e029dea6a327ecc2d6c203a12f85645a9221a776
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-24 23:08:06 +0300

    Added routers for Comment.

--- src/post/routers.py
+++ src/post/routers.py
@@ -4,6 +4,7 @@ from starlette import status
 
 from database.dependencies import get_db
 from src.post import crud, schemas
+from src.post.comment.schemas import CommentCreate, CommentsRead, CommentTree
 from src.user import auth
 from src.user.models import User
 
@@ -16,8 +17,7 @@ router = APIRouter()
     tags=["Post"],
 )
 async def read_posts(db: AsyncSession = Depends(get_db)):
-    posts = await crud.get_posts(db)
-    return [schemas.PostReadDetail.from_orm(post) for post in posts]
+    return await crud.get_posts(db)
 
 
 @router.get(
@@ -26,8 +26,7 @@ async def read_posts(db: AsyncSession = Depends(get_db)):
     tags=["Post"],
 )
 async def post_read_by_id(post_id: int, db: AsyncSession = Depends(get_db)):
-    post = await crud.get_post_by_id(db, post_id)
-    return schemas.PostReadDetail.from_orm(post)
+    return await crud.get_post_by_id(db, post_id)
 
 
 @router.post(
@@ -65,9 +64,71 @@ async def post_update(
     status_code=status.HTTP_204_NO_CONTENT,
     tags=["Post"],
 )
-async def delete_route(
+async def delete_post(
     db: AsyncSession = Depends(get_db),
     post_id: int = None,
     user: User = Depends(auth.current_user),
 ):
     return await crud.post_delete(db=db, post_id=post_id)
+
+
+@router.get(
+    "/post/{post_id}/comments",
+    response_model=list[CommentTree],
+    tags=["Comment"],
+)
+async def get_comments_by_post_id(post_id: int, db: AsyncSession = Depends(get_db)):
+    # comments = await crud.get_comments_by_post(post_id, db)
+    return await crud.get_comments_tree(db, post_id)
+
+
+@router.get(
+    "/comments/{comment_id}",
+    response_model=CommentsRead,
+    tags=["Comment"],
+)
+async def get_children_comments_by_parent_id(
+    comment_id: int, db: AsyncSession = Depends(get_db)
+):
+    return await crud.get_comment_by_comment_id(db, comment_id)
+
+
+@router.get(
+    "/comments/{parent_id}/children",
+    response_model=list[CommentsRead],
+    tags=["Comment"],
+)
+async def get_children_comments_by_parent_id(
+    parent_id: int, db: AsyncSession = Depends(get_db)
+):
+    return await crud.get_children_comments(db, parent_id)
+
+
+@router.post(
+    "/posts/{post_id}/comment",
+    response_model=CommentsRead,
+    tags=["Comment"],
+    status_code=status.HTTP_201_CREATED,
+)
+async def comment_create(
+    post_id: int,
+    comment_data: CommentCreate,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth.current_user),
+):
+    return await crud.create_comment(
+        post_id=post_id, db=db, comment_data=comment_data, user=user
+    )
+
+
+@router.delete(
+    "/comments/{comment_id}",
+    tags=["Comment"],
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_comment(
+    db: AsyncSession = Depends(get_db),
+    comment_id: int = None,
+    user: User = Depends(auth.current_user),
+):
+    return await crud.delete_comment(db=db, comment_id=comment_id)
