307056c28ec0116572cbc0ef06a7fb2a15a4d668
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-25 00:24:17 +0300

    Create analyze_text_toxicity func. for analyze comments.

---
+++ src/services/text_toxicity_analysis.py
@@ -0,0 +1,35 @@
+import logging
+
+from googleapiclient import discovery
+
+from config import settings
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+client = discovery.build(
+    "commentanalyzer",
+    "v1alpha1",
+    developerKey=settings.PERSPECTIVE_API_KEY,
+    discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+    static_discovery=False,
+)
+
+
+async def analyze_text_toxicity(comment_text: str):
+    analyze_request = {
+        'comment': {'text': comment_text},
+        'requestedAttributes': {'TOXICITY': {}}
+    }
+
+    response = client.comments().analyze(body=analyze_request).execute()
+    toxicity_score = response['attributeScores']['TOXICITY']['summaryScore']['value']
+
+    if toxicity_score < 0.2:
+        logger.info(f"Text toxicity level: Low ({toxicity_score:.2f})")
+    elif 0.2 <= toxicity_score < 0.5:
+        logger.info(f"Text toxicity level: Medium ({toxicity_score:.2f})")
+    else:
+        logger.info(f"Text toxicity level: High ({toxicity_score:.2f})")
+
+    return toxicity_score
