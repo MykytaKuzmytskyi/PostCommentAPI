902fd33af1552f7ba99ff99003fecd76673f8328
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-26 08:37:09 +0300

    Added reply_comment to create comment.

--- src/post/crud.py
+++ src/post/crud.py
@@ -11,10 +11,12 @@ from src.post.comment.schemas import CommentCreate, CommentTree
 from src.post.comment.utils import (
     comment_children_create,
     get_max_rgt,
-    create_new_comment,
     shift_comment_tree,
 )
+from src.post.models import Post
+from src.services.celery_app import reply_comment
 from src.services.text_toxicity_analysis import analyze_text_toxicity
+from src.user.models import User
 
 
 async def get_posts(db: AsyncSession):
@@ -90,7 +92,7 @@ async def get_comment_by_comment_id(db: AsyncSession, comment_id: int):
 async def get_children_comments(db: AsyncSession, parent_id: int) -> list[Comment]:
     children_query = select(Comment).where(
         Comment.parent_id == parent_id
-    )  # Обмеження до двох
+    )
     children_result = await db.execute(children_query)
     return children_result.scalars().all()
 
@@ -122,31 +124,44 @@ async def get_comments_tree(db: AsyncSession, post_id: int) -> list[CommentTree]
 
 
 async def create_comment(
-    db: AsyncSession, post_id: int, comment_data: CommentCreate, user
+        db: AsyncSession, post_id: int, comment_data: CommentCreate, user
 ):
+    await get_post_by_id(db, post_id)
+
     toxicity_score = await analyze_text_toxicity(comment_data.content)
     is_blocked = toxicity_score > 0.5
 
     result = await db.execute(select(func.count(Comment.id)))
     comments_count = result.scalar()
     if comments_count == 0:
-        new_comment = await create_new_comment(
-            db, comment_data, post_id, user, is_blocked, lft=1, rgt=2, level=0
+        new_comment = Comment(
+            post_id=post_id,
+            parent_id=comment_data.parent_id,
+            content=comment_data.content,
+            user_id=user.id,
+            lft=1,
+            rgt=2,
+            level=0
         )
     else:
         if comment_data.parent_id is not None:
             new_comment = await comment_children_create(
-                db, post_id, comment_data, user, is_blocked
+                db,
+                post_id,
+                comment_data.parent_id,
+                comment_data.content,
+                user.id,
+                is_blocked
             )
         else:
             max_rgt = await get_max_rgt(db, post_id)
             await shift_comment_tree(db, max_rgt)
-            new_comment = await create_new_comment(
-                db,
-                comment_data,
-                post_id,
-                user,
-                is_blocked,
+            new_comment = Comment(
+                post_id=post_id,
+                parent_id=comment_data.parent_id,
+                content=comment_data.content,
+                user_id=user.id,
+                is_blocked=is_blocked,
                 lft=max_rgt + 1,
                 rgt=max_rgt + 2,
                 level=0,
@@ -155,6 +170,16 @@ async def create_comment(
     db.add(new_comment)
     await db.commit()
     await db.refresh(new_comment)
+
+    user_db = await db.execute(
+        select(User).join(Post).where(Post.id == post_id)
+    )
+    user = user_db.scalar_one_or_none()
+    if user.auto_reply_enabled:
+        reply_comment.apply_async(
+            args=[post_id, new_comment.id, user.id],
+            countdown=user.auto_reply_delay.total_seconds()
+        )
 
     return new_comment
 
@@ -169,15 +194,12 @@ async def delete_comment(db: AsyncSession, comment_id: int):
     await db.execute(
         delete(Comment).where(Comment.lft >= min_lft, Comment.rgt <= max_rgt)
     )
-
     await db.execute(
         update(Comment).where(Comment.lft > max_rgt).values(lft=Comment.lft - width)
     )
-
     await db.execute(
         update(Comment).where(Comment.rgt > max_rgt).values(rgt=Comment.rgt - width)
     )
-
     await db.commit()
 
     return
