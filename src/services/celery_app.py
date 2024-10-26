b797ec45ceea8d714dd9785d552c13e1ba510cbe
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-26 08:36:47 +0300

    Created func. reply_comment.

--- src/services/celery_app.py
+++ src/services/celery_app.py
@@ -1,6 +1,9 @@
-from celery import Celery
+from asgiref.sync import async_to_sync
+from celery import Celery, shared_task
 
 from config import settings
+from database.database import SessionLocal
+from src.post.comment.utils import comment_children_create
 
 celery = Celery(
     'src.services.celery_app',
@@ -16,3 +19,23 @@ celery.conf.update(
     timezone='UTC',
     enable_utc=True,
 )
+
+
+@shared_task
+def reply_comment(post_id, parent_id, user_id):
+    async_to_sync(reply_comment_async)(post_id, parent_id, user_id)
+
+
+async def reply_comment_async(post_id, parent_id, user_id):
+    async with SessionLocal() as db:
+        new_reply = await comment_children_create(
+            db,
+            post_id,
+            parent_id,
+            "RepLY Comment",
+            user_id,
+            is_blocked=False,
+        )
+        db.add(new_reply)
+        await db.commit()
+        await db.refresh(new_reply)
