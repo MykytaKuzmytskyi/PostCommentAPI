4e0210feb4491f74be9fe3a8d46b273a1e169ec8
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-28 17:12:03 +0200

    Added is_blocked field to post model & update migration.

--- src/post/models.py
+++ src/post/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, Column, Text, func, ForeignKey, DateTime, String
+from sqlalchemy import Integer, Column, Text, func, ForeignKey, DateTime, String, Boolean
 from sqlalchemy.orm import relationship
 
 from database.database import Base
@@ -12,6 +12,7 @@ class Post(Base):
     content = Column(Text, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     user_id = Column(Integer, ForeignKey("user.id"))
+    is_blocked = Column(Boolean, default=False)
 
     user = relationship("User", back_populates="posts")
     comments = relationship(
